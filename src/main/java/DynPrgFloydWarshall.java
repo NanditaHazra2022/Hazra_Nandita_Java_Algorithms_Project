26990dc8f665dc4adfef62ffdbf79d330a979507
Nandita Hazra <hazranandita2022@gmail.com>
2022-11-03 11:09:14 +0530

    Modified Java program: DynPrgFloydWarshall.java to remove warnings in the code and added comments.

--- src/main/java/DynPrgFloydWarshall.java
+++ src/main/java/DynPrgFloydWarshall.java
@@ -1,8 +1,7 @@
 /* Java program to demonstrate
 * Dynamic Programming using
-* Floyd-Warshall algorithm.
-* Floyd-Warshall Algorithm
-* is an algorithm
+* Floyd-Warshall algorithm,
+* an algorithm
 * for finding the shortest path
 * between all the pairs
 * of vertices in a weighted graph.
@@ -14,73 +13,99 @@
 // Main class.
 public class DynPrgFloydWarshall
 {   // Start of main class block.
-    final static int INF = 9999, VER = 4;
+    final static int INF = 999999999, VER = 4;  // INF is short for infinity and VER is short for vertex.
 
-    // Implementing Floyd-Warshall algorithm
-    void floydWarshall(int[][] graph) {
-        int[][] matrix = new int[VER][VER];
-        int i, j, k;
+    // Method to implement Floyd-Warshall algorithm.
+    void floydWarshall(int[][] matrixA0)
+    {   // Start of algorithm implementation block.
 
-        for (i = 0;
-             i < VER;
-             i++) {
-            for (j = 0;
-                 j < VER;
-                 j++) {
-                // Re-creating the input array, for element manipulation.
-                matrix[i][j] = graph[i][j];
-            }
-        }
+        // Two dimensional array declaration.
+        int[][] matrixA1 = new int[VER][VER];
 
-        // Adding vertices individually
-        for (k = 0;
+        // Copying weighted graph.
+        System.arraycopy(matrixA0, 0,
+                matrixA1, 0, VER);
+
+        /* The elements in the first column
+         * and the first row are left as they are.
+         * k is the intermediate vertex
+         * in the shortest path
+         * from source (i) to destination (j).
+         * If direct distance
+         * from source (i) to destination (j)
+         * is greater than the path
+         * through the intermediate vertex (k),
+         * then the cell is filled
+         * with A[i][k] + A[k][j]. */
+        for (int k = 0;
              k < VER;
-             k++) {
-            for (i = 0;
+             k++)
+        {   // Start of outer for loop block.
+            for (int i = 0;
                  i < VER;
-                 i++) {
-                for (j = 0;
+                 i++)
+            {   // Start of inner for loop 1 block.
+                for (int j = 0;
                      j < VER;
-                     j++) {
-                    if (matrix[i][k] + matrix[k][j] < matrix[i][j]) {
-                        matrix[i][j]
-                                = matrix[i][k] + matrix[k][j];
+                     j++)
+                {   // Start of inner for loop 2 block.
+                    if (matrixA1[i][k] + matrixA1[k][j] < matrixA1[i][j]) {
+                        matrixA1[i][j]
+                                = matrixA1[i][k] + matrixA1[k][j];
                     }
-                }
-            }
-        }
-        printMatrix(matrix);
-    }
+                }   // End of inner for loop 2 block.
+            }   // End of inner for loop 1 block.
+        }   // End of outer for loop block.
 
-    void printMatrix(int[][] matrix) {
+        // Calling method to print matrix.
+        printMatrix(matrixA1);
+
+    }   // End of algorithm implementation block.
+
+    // Method for printing matrix.
+    void printMatrix(int[][] matrix)
+    {   // Start of printing matrix block.
         for (int i = 0;
              i < VER;
-             ++i) {
+             ++i)
+        {   // Start of outer for loop block.
             for (int j = 0;
                  j < VER;
-                 ++j) {
+                 ++j)
+            {   // Start of inner for loop block.
                 if (matrix[i][j] == INF) {
                     System.out.print("INF ");
                 }
                 else {
                     System.out.print(matrix[i][j] + "  ");
                 }
-            }
+            }   // End of inner for loop block.
             System.out.println();
-        }
-    }
+        }   // End of outer for loop block.
+    }   // End of printing matrix block.
 
     // Starting point of execution.
     public static void main(String[] args)
     {   // Start of execution thread block.
 
-        int[][] graph = { { 0, 3, INF, 5 },
+        /* Creating a matrix A0 of dimension 4 * 4
+        * as 4 is the number of vertices.
+        * The row and the column are indexed
+        * as i and j respectively.
+        * i and j are the vertices of the graph.
+        * Each cell matrixA0[i][j] is filled
+        * with the distance from the ith vertex
+        * to the jth vertex.
+        * If there is no path
+        * from ith vertex to jth vertex,
+        * the cell is left as infinity (INF). */
+        int[][] matrixA0 = { { 0, 3, INF, 5 },
                 { 2, 0, INF, 4 },
                 { INF, 1, 0, INF },
                 { INF, INF, 2, 0 } };
 
         DynPrgFloydWarshall d = new DynPrgFloydWarshall();
-        d.floydWarshall(graph);
+        d.floydWarshall(matrixA0);
 
     }   // End of execution thread block.
 
